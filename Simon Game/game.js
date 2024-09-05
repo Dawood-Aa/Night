b8e62aaec71257cdb0bc0d0587ba8530d8b91b7a
WhiteLion1 <134814834+Dawood-Aa@users.noreply.github.com>
2024-09-05 21:41:53 +0500

    Update game.js

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -15,7 +15,7 @@ $(document).keypress(function() {
   }
 });
 
-$("#level-title").click(function() {
+$(document).click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
