2bdbd998d4ac556bedced812e4a93b8cebcbe56e
WhiteLion1 <134814834+Dawood-Aa@users.noreply.github.com>
2024-09-10 22:59:50 +0500

    Update index.js

--- Randomizer/index.js
+++ Randomizer/index.js
@@ -1,3 +1,6 @@
+
+function BDSM(){
+
     const BDSM = [
         "000001.jpg", "000002.jpg", "000003.jpg", "000004.jpg", "000005.jpg", 
         "000006.jpg", "000007.jpg", "000008.jpg", "000009.jpg", "000010.jpg",
@@ -20,6 +23,13 @@
         "000091.jpg", "000092.jpg", "000093.jpg", "000094.jpg", "000095.jpg", 
         "000096.jpg", "000097.jpg", "000098.jpg", "000099.jpg", "000100.jpg"
     ];
+
+
+   var random = Math.floor(Math.random()*100)
+   $(".image").attr("src", "Randomizer/BDSMimages/" + BDSM[random])
+}
+
+function Femdom(){
 
     const Femdom = [
         'Fem_Part_1_Jun_16_146.jpg', 'Fem_Part_1_Jun_16_147.jpg', 'Fem_Part_1_Jun_16_148.jpg', 
@@ -57,28 +67,81 @@
         'Fem_Part_1_Jun_16_242.jpeg', 'Fem_Part_1_Jun_16_243.jpeg', 'Fem_Part_1_Jun_16_244.jpeg', 
         'Fem_Part_1_Jun_16_245.jpg'
     ];
-
-function BDSM() {
-    const randomIndex = Math.floor(Math.random() * BDSM.length);
-    $(".image").attr("src", "Randomizer/BDSMimages/" + BDSM[randomIndex]);
+    var random = Math.floor(Math.random()*100)
+    $(".image").attr("src", "Randomizer/FemdomImages/" + Femdom[random]) 
 }
 
-function Femdom() {
-    const randomIndex = Math.floor(Math.random() * Femdom.length);
-    $(".image").attr("src", "Randomizer/FemdomImages/" + Femdom[randomIndex]);
-}
 
-function Randomimg() {
-    const rand = Math.floor(Math.random() * 2 + 1);
+function Randomimg(){
+    const BDSM = [
+        "000001.jpg", "000002.jpg", "000003.jpg", "000004.jpg", "000005.jpg", 
+        "000006.jpg", "000007.jpg", "000008.jpg", "000009.jpg", "000010.jpg",
+        "000011.jpg", "000012.jpg", "000013.jpg", "000014.jpg", "000015.jpg", 
+        "000016.jpg", "000017.jpg", "000018.jpg", "000019.jpg", "000020.jpg",
+        "000021.jpg", "000022.jpg", "000023.jpg", "000024.jpg", "000025.jpg", 
+        "000026.jpg", "000027.jpg", "000028.jpg", "000029.jpg", "000030.jpg",
+        "000031.jpg", "000032.jpg", "000033.jpg", "000034.jpg", "000035.jpg", 
+        "000036.jpg", "000037.jpg", "000038.jpg", "000039.jpg", "000040.jpg",
+        "000041.jpg", "000042.jpg", "000043.jpg", "000044.jpg", "000045.jpg", 
+        "000046.jpg", "000047.jpg", "000048.jpg", "000049.jpg", "000050.jpg",
+        "000051.jpg", "000052.jpg", "000053.jpg", "000054.jpg", "000055.jpg", 
+        "000056.jpg", "000057.jpg", "000058.jpg", "000059.jpg", "000060.jpg",
+        "000061.jpg", "000062.jpg", "000063.jpg", "000064.jpg", "000065.jpg", 
+        "000066.jpg", "000067.jpg", "000068.jpg", "000069.jpg", "000070.jpg",
+        "000071.jpg", "000072.jpg", "000073.jpg", "000074.jpg", "000075.jpg", 
+        "000076.jpg", "000077.jpg", "000078.jpg", "000079.jpg", "000080.jpg",
+        "000081.jpg", "000082.jpg", "000083.jpg", "000084.jpg", "000085.jpg", 
+        "000086.jpg", "000087.jpg", "000088.jpg", "000089.jpg", "000090.jpg",
+        "000091.jpg", "000092.jpg", "000093.jpg", "000094.jpg", "000095.jpg", 
+        "000096.jpg", "000097.jpg", "000098.jpg", "000099.jpg", "000100.jpg"
+    ];
+    const Femdom = [
+        'Fem_Part_1_Jun_16_146.jpg', 'Fem_Part_1_Jun_16_147.jpg', 'Fem_Part_1_Jun_16_148.jpg', 
+        'Fem_Part_1_Jun_16_149.jpg', 'Fem_Part_1_Jun_16_150.gif', 'Fem_Part_1_Jun_16_151.jpg', 
+        'Fem_Part_1_Jun_16_152.jpg', 'Fem_Part_1_Jun_16_153.jpg', 'Fem_Part_1_Jun_16_154.jpg', 
+        'Fem_Part_1_Jun_16_155.jpg', 'Fem_Part_1_Jun_16_156.jpg', 'Fem_Part_1_Jun_16_157.jpg', 
+        'Fem_Part_1_Jun_16_158.jpg', 'Fem_Part_1_Jun_16_159.jpg', 'Fem_Part_1_Jun_16_160.jpg', 
+        'Fem_Part_1_Jun_16_161.jpg', 'Fem_Part_1_Jun_16_162.jpg', 'Fem_Part_1_Jun_16_163.jpg', 
+        'Fem_Part_1_Jun_16_164.jpg', 'Fem_Part_1_Jun_16_165.gif', 'Fem_Part_1_Jun_16_166.jpg', 
+        'Fem_Part_1_Jun_16_167.jpg', 'Fem_Part_1_Jun_16_168.jpg', 'Fem_Part_1_Jun_16_169.jpg', 
+        'Fem_Part_1_Jun_16_170.jpg', 'Fem_Part_1_Jun_16_171.jpg', 'Fem_Part_1_Jun_16_172.jpg', 
+        'Fem_Part_1_Jun_16_173.jpg', 'Fem_Part_1_Jun_16_174.jpg', 'Fem_Part_1_Jun_16_175.jpg', 
+        'Fem_Part_1_Jun_16_176.jpg', 'Fem_Part_1_Jun_16_177.jpg', 'Fem_Part_1_Jun_16_178.jpg', 
+        'Fem_Part_1_Jun_16_179.jpg', 'Fem_Part_1_Jun_16_180.jpg', 'Fem_Part_1_Jun_16_181.jpg', 
+        'Fem_Part_1_Jun_16_182.jpg', 'Fem_Part_1_Jun_16_183.jpg', 'Fem_Part_1_Jun_16_184.jpg', 
+        'Fem_Part_1_Jun_16_185.jpg', 'Fem_Part_1_Jun_16_186.jpg', 'Fem_Part_1_Jun_16_187.jpg', 
+        'Fem_Part_1_Jun_16_188.jpg', 'Fem_Part_1_Jun_16_189.jpg', 'Fem_Part_1_Jun_16_190.jpg', 
+        'Fem_Part_1_Jun_16_191.jpg', 'Fem_Part_1_Jun_16_192.jpg', 'Fem_Part_1_Jun_16_193.jpg', 
+        'Fem_Part_1_Jun_16_194.jpg', 'Fem_Part_1_Jun_16_195.jpg', 'Fem_Part_1_Jun_16_196.jpg', 
+        'Fem_Part_1_Jun_16_197.jpg', 'Fem_Part_1_Jun_16_198.jpg', 'Fem_Part_1_Jun_16_199.jpg', 
+        'Fem_Part_1_Jun_16_200.jpg', 'Fem_Part_1_Jun_16_201.jpg', 'Fem_Part_1_Jun_16_202.jpg', 
+        'Fem_Part_1_Jun_16_203.jpg', 'Fem_Part_1_Jun_16_204.jpg', 'Fem_Part_1_Jun_16_205.jpg', 
+        'Fem_Part_1_Jun_16_206.jpg', 'Fem_Part_1_Jun_16_207.jpg', 'Fem_Part_1_Jun_16_208.jpg', 
+        'Fem_Part_1_Jun_16_209.jpg', 'Fem_Part_1_Jun_16_210.jpg', 'Fem_Part_1_Jun_16_211.jpg', 
+        'Fem_Part_1_Jun_16_212.jpeg', 'Fem_Part_1_Jun_16_213.jpg', 'Fem_Part_1_Jun_16_214.jpeg', 
+        'Fem_Part_1_Jun_16_215.jpeg', 'Fem_Part_1_Jun_16_216.jpeg', 'Fem_Part_1_Jun_16_217.jpeg', 
+        'Fem_Part_1_Jun_16_218.jpg', 'Fem_Part_1_Jun_16_219.jpg', 'Fem_Part_1_Jun_16_220.jpg', 
+        'Fem_Part_1_Jun_16_221.jpg', 'Fem_Part_1_Jun_16_222.jpeg', 'Fem_Part_1_Jun_16_223.gif', 
+        'Fem_Part_1_Jun_16_224.jpg', 'Fem_Part_1_Jun_16_225.jpeg', 'Fem_Part_1_Jun_16_226.jpg', 
+        'Fem_Part_1_Jun_16_227.jpg', 'Fem_Part_1_Jun_16_228.jpg', 'Fem_Part_1_Jun_16_229.jpg', 
+        'Fem_Part_1_Jun_16_230.jpg', 'Fem_Part_1_Jun_16_231.jpeg', 'Fem_Part_1_Jun_16_232.jpg', 
+        'Fem_Part_1_Jun_16_233.jpeg', 'Fem_Part_1_Jun_16_234.jpeg', 'Fem_Part_1_Jun_16_235.jpg', 
+        'Fem_Part_1_Jun_16_236.jpeg', 'Fem_Part_1_Jun_16_237.jpeg', 'Fem_Part_1_Jun_16_238.jpeg', 
+        'Fem_Part_1_Jun_16_239.jpeg', 'Fem_Part_1_Jun_16_240.jpeg', 'Fem_Part_1_Jun_16_241.jpeg', 
+        'Fem_Part_1_Jun_16_242.jpeg', 'Fem_Part_1_Jun_16_243.jpeg', 'Fem_Part_1_Jun_16_244.jpeg', 
+        'Fem_Part_1_Jun_16_245.jpg'
+    ];
+    var rand = Math.floor(Math.random()*2+1)
 
-    if (rand === 1) {
-        Femdom();
-    } else if (rand === 2) {
-        BDSM();
+    if(rand===1){
+    var random = Math.floor(Math.random()*100)
+    $(".image").attr("src", "Randomizer/FemdomImages/" + Femdom[random]) 
+}else if(rand===2){
+    var random = Math.floor(Math.random()*100)
+    $(".image").attr("src", "Randomizer/BDSMimages/" + BDSM[random]) 
     }
 }
 
-// Attach event listeners
 $('#changeBDSM').click(BDSM);
 $('#changeFemdom').click(Femdom);
-$('#randomimg').click(Randomimg);
+$('#randomimg').click(Randomimg)
